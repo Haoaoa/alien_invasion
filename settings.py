66be1aba45856d3a3221d2063cb02a86e9e88460
Haoaoa <364793098@qq.com>
2017-08-09 16:29:10 +0800

    change

--- settings.py
+++ settings.py
@@ -6,8 +6,13 @@ class Settings():
         self.screen_height = 700
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.ship_limit = 3
+        self.bullet_speed_factor = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = 60,60,60
         self.bullets_allowed = 8
+        self.alien_speed_factor = 1
+        self.fleet_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction =1
